53e1d8891bcf368ca224e2aff2d0d6cdd5a9f14f
Matias Acosta <acostamatias.1989@gmail.com>
2020-09-19 22:11:33 -0300

    Script para generar .py de .qrc o .ui

---
+++ scripts/script_mw.bat
@@ -0,0 +1,17 @@
+@echo off
+echo Generaremos los archivos .qrc y .ui a .py.
+pause
+pyrcc5 -o ..\src\resources.py ..\qrc\resources.qrc 
+pyuic5 ..\ui\form_alumno_update.ui -o ..\src\views\alumno\form_alumno_update.py
+pyuic5 ..\ui\form_socio_update.ui -o ..\src\views\socio\form_socio_update.py 
+pyuic5 ..\ui\form_socio_base.ui -o ..\src\views\socio\form_socio_base.py 
+pyuic5 ..\ui\form_window_default.ui -o ..\src\views\main_window\form_window_default.py
+pyuic5 ..\ui\login_base.ui -o ..\src\views\login\login_base.py
+
+pyuic5 ..\ui\main_window_base.ui -o ..\src\views\main_window\main_window_base.py
+pyuic5 ..\ui\table_view_alumno.ui -o ..\src\views\alumno\table_view_alumno.py
+pyuic5 ..\ui\table_view_socio.ui -o ..\src\views\socio\table_view_socio.py
+
+#sed -i -e 's/resources_rc/resources/g' ..\src\views\login\login_base.py 
+CD ..\src\views\login\
+(Get-Content login_base.py) -replace 'resources', 'resources_rc' | Out-File -encoding ASCII login_base.py
