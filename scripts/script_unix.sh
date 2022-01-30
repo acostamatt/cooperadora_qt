b7e5ebc445d2419a22110956c0e644c11e5f0761
Mati Acosta <acostamatias.1989@gmail.com>
2022-01-30 12:24:17 -0300

    Add ui form cobranza file.

--- scripts/script_unix.sh
+++ scripts/script_unix.sh
@@ -3,6 +3,8 @@ mkdir -p ../src/views/base
 pyrcc5 -o ../src/resources.py ../qrc/resources.qrc 
 pyuic5 ../ui/form_alumno_base.ui -o ../src/views/base/form_alumno_base.py
 pyuic5 ../ui/form_socio_base.ui -o ../src/views/base/form_socio_base.py
+pyuic5 ../ui/form_cobranza_base.ui -o ../src/views/base/form_cobranza_base.py
+pyuic5 ../ui/form_anular_cobranza_base.ui -o ../src/views/base/form_anular_cobranza_base.py
 pyuic5 ../ui/table_window_default.ui -o ../src/views/base/table_window_default.py
 pyuic5 ../ui/login_base.ui -o ../src/views/base/login_base.py
 
